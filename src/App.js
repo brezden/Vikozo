a7279b30bbc74e025272284c755f3b1c792c1c4b
brezden <callumbrezden@gmail.com>
2023-03-23 14:09:22 -0700

    Created Access to Budgets
    Used React Bootstrap to allow the user to select which budget they want to open. This includes a update button for the name and purpose of the budget. This button has not been configured yet but it is viewable.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import "./index.css";
-import User from "./components/User";
+import Budget from "./components/Budget";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const showUser = true;
-  const [role, setRole] = useState();
+  const [users, setUsers] = useState([
+    {
+      name: "Personal",
+      purpose: "Used for personal budgeting",
+      img: "https://cdn-icons-png.flaticon.com/512/1511/1511168.png",
+    },
+    {
+      name: "Company",
+      purpose: "All expenses for company",
+      img: "https://cdn-icons-png.flaticon.com/512/1511/1511164.png",
+    },
+    {
+      name: "Investments",
+      purpose: "Stock and real estate investments",
+      img: "https://cdn-icons-png.flaticon.com/512/1043/1043465.png",
+    },
+  ]);
 
   return (
-    <div className="App bg-gray-800">
-      {showUser ? (
-        <>
-          <input
-            type="text"
-            onChange={(e) => {
-              setRole(e.target.value);
-            }}
-          />
-          <User name="Callum" role="Tester" />
-          <User name="Bob" role={role} />
-        </>
-      ) : (
-        <p>Hello</p>
-      )}
+    <div className="App">
+      <>
+        <div className="flex flex-wrap justify-center">
+          {users.map((user) => {
+            return (
+              <Budget
+                key={uuidv4()}
+                name={user.name}
+                purpose={user.purpose}
+                img={user.img}
+              />
+            );
+          })}
+        </div>
+      </>
     </div>
   );
 }
